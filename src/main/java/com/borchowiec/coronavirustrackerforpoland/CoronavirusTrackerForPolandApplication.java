696a46f5c7ae7ec96ea1d6d72348ff4b407df4d8
borchowiec <borchowiecpatryk@gmail.com>
2020-03-24 20:08:23 +0100

    Init commit

---
+++ src/main/java/com/borchowiec/coronavirustrackerforpoland/CoronavirusTrackerForPolandApplication.java
@@ -0,0 +1,13 @@
+package com.borchowiec.coronavirustrackerforpoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerForPolandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusTrackerForPolandApplication.class, args);
+    }
+
+}
