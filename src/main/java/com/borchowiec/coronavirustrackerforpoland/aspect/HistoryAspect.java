9bc38fcfd6a2566fe5b914ac4462da21a36b19b8
borchowiec <borchowiecpatryk@gmail.com>
2020-03-31 12:39:12 +0200

    Previous commit didn't contain every needed files.

--- src/main/java/com/borchowiec/coronavirustrackerforpoland/aspect/HistoryAspect.java
+++ src/main/java/com/borchowiec/coronavirustrackerforpoland/aspect/HistoryAspect.java
@@ -1,4 +1,30 @@
 package com.borchowiec.coronavirustrackerforpoland.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class HistoryAspect {
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Around("execution(* com.borchowiec.coronavirustrackerforpoland.service.HistoryService.updateHistoryList())")
+    public void updateHistoryListMethod(ProceedingJoinPoint proceedingJoinPoint){
+        logger.info("Starting updating history list...");
+        boolean success = true;
+
+        try {
+            proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            logger.error("Cannot update history list: " + throwable.getMessage());
+            success = false;
+        }
+
+        if (success) {
+            logger.info("History list has been updated");
+        }
+    }
 }
