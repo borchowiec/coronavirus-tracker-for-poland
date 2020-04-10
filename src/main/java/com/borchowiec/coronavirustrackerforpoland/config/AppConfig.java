cfd1b310d95b843bd0ab207014f54f6dd37fd359
borchowiec <borchowiecpatryk@gmail.com>
2020-04-11 00:49:15 +0200

    Set up default locale.

--- src/main/java/com/borchowiec/coronavirustrackerforpoland/config/AppConfig.java
+++ src/main/java/com/borchowiec/coronavirustrackerforpoland/config/AppConfig.java
@@ -3,7 +3,15 @@ package com.borchowiec.coronavirustrackerforpoland.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+
 @Configuration
 @EnableScheduling
 public class AppConfig {
+
+    @PostConstruct
+    public void init() {
+        Locale.setDefault(new Locale("pl"));
+    }
 }
